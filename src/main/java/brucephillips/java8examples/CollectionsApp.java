f089a0b8640fc6e2d985615c94349a4d259d70f6
brucephillips <brucephillips@10.119.87.65>
2015-08-11 13:45:21 -0500

    Added getting single element from a collection.

--- src/main/java/brucephillips/java8examples/CollectionsApp.java
+++ src/main/java/brucephillips/java8examples/CollectionsApp.java
@@ -8,6 +8,9 @@ package brucephillips.java8examples;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.function.Predicate;
+import java.util.function.Function;
+import java.util.Optional;
 
 
 /**
@@ -15,26 +18,48 @@ import java.util.stream.Collectors;
  * @author brucephillips
  */
 public class CollectionsApp {
-    
-    private final static List<String> friends = 
-    Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
-    
-    public static void main(String [] args) {
-        
-        friends.forEach( name -> System.out.print(name + " "));
-        
+
+    private final static List<String> friends
+            = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+
+    public static void main(String[] args) {
+
+        friends.forEach(name -> System.out.print(name + " "));
+
         System.out.println();
 
-        friends.stream().map( name -> name.toUpperCase() ).forEach( name -> System.out.print(name + " ") );
-        
+        friends.stream().map(name -> name.toUpperCase()).forEach(name -> System.out.print(name + " "));
+
         System.out.println();
-        
+
         final List<String> startsWithN = friends.stream().filter(name -> name.startsWith("N")).collect(Collectors.toList());
-        
-        startsWithN.forEach( name -> System.out.print(name + " "));
-        
+
+        startsWithN.forEach(name -> System.out.print(name + " "));
+
         System.out.println();
+
+        final Predicate<String> startsWithNPred = name -> name.startsWith("N");
+
+        final long countFriends = friends.stream().filter(startsWithNPred).count();
+
+        System.out.println("Number of friends start with N is " + countFriends);
+
+        final Function<String, Predicate<String>> startsWithLetter
+                = (String letter) -> {
+                    Predicate<String> checkStarts = (String name) -> name.startsWith(letter);
+                    return checkStarts;
+                };
+
+        final long countFriendsB = friends.stream().filter(startsWithLetter.apply("B")).count();
+
+        System.out.println("Number of friends start with B is " + countFriendsB);
         
+        final String startingLetter = "S";
+        
+        final Optional<String> foundName = friends.stream().filter(name -> name.startsWith(startingLetter)).findFirst() ;
+        
+        System.out.println( String.format("A name starting with %s: %s", startingLetter, foundName.orElse("No name found"))) ;
+
     }
-    
+
 }
